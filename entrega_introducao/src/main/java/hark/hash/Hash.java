b3b38913b04c7dcb2e1ec20c2aa0d2530aac4b3d
Pedro Minicz <pedrominicz@gmail.com>
2020-09-18 21:07:46 -0300

    Add `Hash.Type` enum.

--- entrega_introducao/src/main/java/hark/hash/Hash.java
+++ entrega_introducao/src/main/java/hark/hash/Hash.java
@@ -6,10 +6,12 @@ public interface Hash {
     public String hashMessage(final String message);
 
     // Hash with a specifig algorithm.
-    public String hashMessage(final String hashType, final String message);
+    public String hashMessage(final Type hashType, final String message);
 
-    public static final String MD5 = "MD5";
-    public static final String SHA1 = "SHA1";
-    public static final String SHA256 = "SHA256";
+    enum Type {
+        MD5,
+        SHA1,
+        SHA256
+    }
     
 }
--- entrega_introducao/src/test/java/hark/hash/HashTest.java
+++ entrega_introducao/src/test/java/hark/hash/HashTest.java
@@ -12,7 +12,7 @@ public class HashTest {
         final String message = "Hello, world!";
 
         final String expected = "6cd3556deb0da54bca060b4c39479839";
-        final String returned = hash.hashMessage(Hash.MD5, message);
+        final String returned = hash.hashMessage(Hash.Type.MD5, message);
 
         assertEquals("Test MD5 hash", expected, returned);
     }
@@ -22,7 +22,7 @@ public class HashTest {
         final String message = "Hello, world!";
 
         final String expected = "943a702d06f34599aee1f8da8ef9f7296031d699";
-        final String returned = hash.hashMessage(Hash.MD5, message);
+        final String returned = hash.hashMessage(Hash.Type.MD5, message);
 
         assertEquals("Test SHA1 hash", expected, returned);
     }
@@ -32,7 +32,7 @@ public class HashTest {
         final String message = "Hello, world!";
 
         final String expected = "315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3";
-        final String returned = hash.hashMessage(Hash.MD5, message);
+        final String returned = hash.hashMessage(Hash.Type.MD5, message);
 
         assertEquals("Test SHA256 hash", expected, returned);
     }
