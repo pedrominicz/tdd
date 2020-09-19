a658ebec823a13847154aa5c6536ca3f7531511a
Pedro Minicz <pedrominicz@gmail.com>
2020-09-18 21:25:24 -0300

    Improve test.

--- entrega_introducao/src/test/java/hark/hash/HashTest.java
+++ entrega_introducao/src/test/java/hark/hash/HashTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.*;
 
 public class HashTest {
 
-    private Hash hash = new HashImpl();
+    private final Hash hash = new HashImpl();
+    
+    private final String message = "Hello, world!";
+
 
     @Test
     public void testMd5() {
-        final String message = "Hello, world!";
-
         final String expected = "6cd3556deb0da54bca060b4c39479839";
         final String returned = hash.hashMessage(Hash.Type.MD5, message);
 
@@ -19,8 +20,6 @@ public class HashTest {
 
     @Test
     public void testSha1() {
-        final String message = "Hello, world!";
-
         final String expected = "943a702d06f34599aee1f8da8ef9f7296031d699";
         final String returned = hash.hashMessage(Hash.Type.SHA1, message);
 
@@ -29,8 +28,6 @@ public class HashTest {
 
     @Test
     public void testSha256() {
-        final String message = "Hello, world!";
-
         final String expected = "315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3";
         final String returned = hash.hashMessage(Hash.Type.SHA256, message);
 
