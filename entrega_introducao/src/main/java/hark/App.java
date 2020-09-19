4d51c60c388e6a6d3389e36c438ad95068655eeb
Pedro Minicz <pedrominicz@gmail.com>
2020-09-18 21:26:04 -0300

    Add Gson.

--- entrega_introducao/src/main/java/hark/App.java
+++ entrega_introducao/src/main/java/hark/App.java
@@ -1,5 +1,7 @@
 package hark;
 
+import com.google.gson.Gson;
+
 import hark.hash.Hash;
 import hark.hash.HashImpl;
 
@@ -7,12 +9,16 @@ public class App {
 
     private final static Hash hash = new HashImpl();
 
-    public static void main(String[] args) {
+    private final static Gson gson = new Gson();
+
+    public static void main(final String[] args) {
         final String message = "Hello, world!";
 
         System.out.println(hash.hashMessage(Hash.Type.MD5, message));
         System.out.println(hash.hashMessage(Hash.Type.SHA1, message));
         System.out.println(hash.hashMessage(Hash.Type.SHA256, message));
+
+        System.out.println(gson.toJson(hash));
     }
 
 }
