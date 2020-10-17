d96c287bb77c8e28cf8dd48a5c019e95b9dbf6aa
Pedro Minicz <pedrominicz@gmail.com>
2020-10-17 12:55:23 -0300

    Chore: change imports to JUnit 5.

--- entregas_testes_funcionais/particoes_de_equivalencia/src/test/java/hark/hash/HashTest.java
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/test/java/hark/hash/HashTest.java
@@ -1,7 +1,8 @@
 package hark.hash;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class HashTest {
 
