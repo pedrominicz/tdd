835dcba2a45afea22855fabf35a1e53b3e7fece0
Pedro Minicz <pedrominicz@gmail.com>
2020-10-17 15:12:55 -0300

    Add mock `verifyMessage` implementation.

--- entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/hark/hash/HashImpl.java
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/hark/hash/HashImpl.java
@@ -7,7 +7,7 @@ public class HashImpl implements Hash {
     }
 
     @Override
-    public String hashMessage(Type hashType, String message) {
+    public String hashMessage(final Type hashType, final String message) {
         switch (hashType) {
             case MD5:
                 return "6cd3556deb0da54bca060b4c39479839";
@@ -21,8 +21,35 @@ public class HashImpl implements Hash {
     }
 
     @Override
-    public boolean verifyMessage(Type hashType, String checksum, String message) {
-        return false;
+    public boolean verifyMessage(final Type hashType, final String checksum, final String message) {
+        switch (hashType) {
+            case MD5:
+                if (checksum.equals("6cd3556deb0da54bca060b4c39479839")) {
+                    return true;
+                } else if (checksum.equals("00000000000000000000000000000000")) {
+                    return false;
+                } else {
+                    throw new HashException();
+                }
+            case SHA1:
+                if (checksum.equals("943a702d06f34599aee1f8da8ef9f7296031d699")) {
+                    return true;
+                } else if (checksum.equals("0000000000000000000000000000000000000000")) {
+                    return false;
+                } else {
+                    throw new HashException();
+                }
+            case SHA256:
+                if (checksum.equals("315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3")) {
+                    return true;
+                } else if (checksum.equals("0000000000000000000000000000000000000000000000000000000000000000")) {
+                    return false;
+                } else {
+                    throw new HashException();
+                }
+        }
+
+        throw new RuntimeException("Unreachable");
     }
 
 }
