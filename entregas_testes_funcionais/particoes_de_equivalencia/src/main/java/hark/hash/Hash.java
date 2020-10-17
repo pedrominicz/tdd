262ef4977cece9587a454f4c28bc45fe073bb38a
Pedro Minicz <pedrominicz@gmail.com>
2020-10-17 12:51:19 -0300

    Import red phase from `entrega_introducao`.

---
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/main/java/hark/hash/Hash.java
@@ -0,0 +1,20 @@
+package hark.hash;
+
+public interface Hash {
+
+    // Hash with the default algorithm.
+    public String hashMessage(final String message);
+
+    // Hash with a specifig algorithm.
+    public String hashMessage(final Type hashType, final String message);
+    
+    // Verify with a specifig algorithm.
+    public String verifyMessage(final Type hashType, final String checksum, final String message);
+
+    enum Type {
+        MD5,
+        SHA1,
+        SHA256
+    }
+    
+}
---
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/test/java/hark/hash/HashTest.java
@@ -0,0 +1,78 @@
+package hark.hash;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HashTest {
+
+    private Hash hash;
+
+    @Test
+    public void testMd5() {
+        final String message = "Hello, world!";
+
+        final String expected = "6cd3556deb0da54bca060b4c39479839";
+        final String returned = hash.hashMessage(Hash.Type.MD5, message);
+
+        assertEquals("Test MD5 hash", expected, returned);
+    }
+
+    @Test
+    public void testSha1() {
+        final String message = "Hello, world!";
+
+        final String expected = "943a702d06f34599aee1f8da8ef9f7296031d699";
+        final String returned = hash.hashMessage(Hash.Type.MD5, message);
+
+        assertEquals("Test SHA1 hash", expected, returned);
+    }
+
+    @Test
+    public void testSha256() {
+        final String message = "Hello, world!";
+
+        final String expected = "315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3";
+        final String returned = hash.hashMessage(Hash.Type.MD5, message);
+
+        assertEquals("Test SHA256 hash", expected, returned);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
