1303c7bb9056e450cd004dd6fd21f95aee9df7a4
Pedro Minicz <pedrominicz@gmail.com>
2021-02-12 13:20:10 -0300

    Test form.

--- entregas_testes_funcionais/teste_api_aluguel/aluguebug.py
+++ entregas_testes_funcionais/teste_api_aluguel/aluguebug.py
@@ -10,14 +10,19 @@ driver.get('https://aluguebug.herokuapp.com/form')
 dia = driver.find_element_by_id('dia')
 nominal = driver.find_element_by_id('valor_nominal')
 calculado = driver.find_element_by_id('resposta')
+calcular = driver.find_element_by_id('botao')
 
-def send_hello(elem):
+def send(elem, value):
     elem.clear()
-    elem.send_keys('Hello, world!')
+    elem.send_keys(value)
 
-send_hello(dia)
-send_hello(nominal)
-send_hello(calculado)
+send(nominal, str(100.0))
 
-sleep(10)
+for i in range(30):
+    send(dia, str(i + 1))
+
+    calcular.click()
+    sleep(0.5)
+    print(i + 1, calculado.get_attribute('value'))
+
 driver.close()
